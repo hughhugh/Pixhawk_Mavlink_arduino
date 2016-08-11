43449930de8ee934ed8b2482d224e6dac94b0d50
hughhugh <478345869@163.com>
2016-08-11 23:11:27 +0800

    example

---
+++ Pixhawk-Arduino-Mavlink-codes/Mav_check/mav_check/mav_check.ino
@@ -0,0 +1,31 @@
+
+#include "C:\Users\Administrator\Desktop\arduino-1.6.0-windows\arduino-1.6.0\libraries\mavlink\include\mavlink.h"
+
+void setup()
+{
+  Serial.begin(57600);
+  while (!Serial)
+  {
+    ; // wait for port to connect.
+  }
+  Serial1.begin(57600);
+  while (!Serial1)
+  {
+    ; // wait for port to connect.
+  }
+  
+  Serial.println("Ready");
+  
+}
+void loop()
+{
+  while (Serial1.available() > 0 ) 
+  {
+    uint8_t c = Serial1.read();
+   Serial.println(c);
+   // Serial.print("Num Bytes: ");
+  //  Serial.println(Serial1.available());
+  }
+ 
+  Serial.println("Not yet");
+}
